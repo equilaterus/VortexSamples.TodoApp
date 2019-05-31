685642646955a10118e49143e6ce9481cedc695a
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-05-30 22:49:30 -0500

    Added context

---
+++ src/backend/TodoApp.Backend/TodoApp.Domain/Infrastructure/Data/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApp.Domain.Models;
+
+namespace TodoApp.Domain.Infrastructure.Data
+{
+    public class Context : DbContext
+    {
+        public DbSet<Todo> Todos { get; set; }
+
+        public Context(DbContextOptions<DbContext> options) : base(options)
+        {
+
+        }
+    }
+}
