9bedc49fd275914d8ccf85e558ce077182134dd0
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-05-30 22:49:11 -0500

    Added todo repository

---
+++ src/backend/TodoApp.Backend/TodoApp.Domain/Infrastructure/Repositories/ITodoRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Domain.Models;
+
+namespace TodoApp.Domain.Infrastructure.Repositories
+{
+    public interface ITodoRepository
+    {
+        Task<List<Todo>> FindAllAsync();
+
+        Task<List<Todo>> FindAsync(Predicate<Todo> filter);
+
+        Task UpdateAsync(Todo entity);
+
+        Task CreateAsync(Todo entity);
+
+        Task DeleteAsync(Todo entity);
+    }
+}
---
+++ src/backend/TodoApp.Backend/TodoApp.Domain/Infrastructure/Repositories/TodoRepository.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TodoApp.Domain.Models;
+
+namespace TodoApp.Domain.Infrastructure.Repositories
+{
+    public class TodoRepository : ITodoRepository
+    {
+        public Task CreateAsync(Todo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(Todo entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Todo>> FindAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Todo>> FindAsync(Predicate<Todo> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(Todo entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
