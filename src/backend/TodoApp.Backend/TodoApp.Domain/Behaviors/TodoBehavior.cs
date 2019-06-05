17f2dab4e9a29002fe0a9a7b12e55e0d7253d7fe
Juan Felipe Cañizares Corrales <pipecaniza@outlook.com>
2019-06-05 13:54:09 -0500

    Added TodoBehavior

---
+++ src/backend/TodoApp.Backend/TodoApp.Domain/Behaviors/TodoBehavior.cs
@@ -0,0 +1,25 @@
+﻿using Equilaterus.Vortex;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using TodoApp.Domain.Models;
+
+namespace TodoApp.Domain.Behaviors
+{
+    public static class TodoBehavior
+    {
+        public static Maybe<Todo> TryUpdate(Todo persitedTodo, Todo newTodo)
+        {
+            return persitedTodo != null ?
+                new Maybe<Todo>(new Todo(newTodo)) :
+                new Maybe<Todo>();
+        }
+
+        public static Maybe<Todo> TryDelete(Todo persitedTodo)
+        {
+            return persitedTodo != null ?
+                new Maybe<Todo>(new Todo(persitedTodo)) :
+                new Maybe<Todo>();
+        }
+    }
+}
